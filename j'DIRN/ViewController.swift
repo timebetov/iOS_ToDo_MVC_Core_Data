94e4f6be82636255f92f94bfadeec6e78aa47ed3
timebetovr <timebetov@gmail.com>
2023-01-11 12:42:06 +0500

    Initial Commit

---
+++ j'DIRN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  j'DIRN
+//
+//  Created by Рахат Султаналиулы on 11.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
